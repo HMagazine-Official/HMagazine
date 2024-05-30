bdebd78876c8c67eb60fb7a22cdeadb2f9c72ce2
DodoNehir <rojalaurant@gmail.com>
2024-05-30 11:54:07 +0900

    [init] add springboot project

---
+++ findexhibition/src/test/java/com/example/findexhibition/FindexhibitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findexhibition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindexhibitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
